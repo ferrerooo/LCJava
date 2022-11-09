1c3f5b915a74f2fc012aefa28b17b38f9dd72276
ferrerooo <ferrerooo@gmail.com>
2022-11-09 11:46:03 -0800

    update

---
+++ .vscode/Util/Util_Stack.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import java.util.*;
+
+public class Util_Stack {
+
+    /*
+     * 
+     * empty() – Tests if this stack is empty.
+     * peek() – Looks at the object at the top of this stack without removing itfrom the stack.
+     * pop() – Removes the object at the top of this stack and returns that object as the value of this function.
+     * push(E item) – Pushes an item onto the top of this stack.
+     * int search(Object o) – Returns the 1-based position where an object is on this stack.
+     */
+
+     @Test
+     public void test() {
+
+        Stack<String> stack2 = new Stack<String>();
+
+        stack2.size();
+        
+     }
+}
---
+++ .vscode/Util/Util_String.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+public class Util_String {
+
+    @Test
+    public void test() {
+
+        // https://www.geeksforgeeks.org/split-string-java-examples/?ref=lbp
+
+        String s = "GeeksforGeeks";
+        s.length();
+        
+        s.charAt(0);
+
+        char[] gfg = s.toCharArray();
+        
+        char a[] = { 'g', 'e', 'e', 'k', 's', 'f', 'o', 'r', 'g', 'e', 'e', 'k', 's' };
+        String s2 = new String(a);
+
+        String str = "geekss@for@geekss";
+        String[] arrOfStr = str.split("@", 2);
+        /*
+         * Regex Limit Result
+         * 
+         * @ 2 {“geekss”, ”for@geekss”}
+         * @ 5 {“geekss”, ”for”, ”geekss”}
+         * @ -2 {“geekss”, ”for”, ”geekss”}
+         * s 5 {“geek”, ”“, “@for@geek”, “”, “”}
+         * s -2 {“geek”, ” “, ” “, “@for@geek”, “”, “”}
+         * s 0 {“geek”, ””, ”@for@geek”}
+         */
+    }
+
+}

c86dac5025a95807c7e068592eeed448b2dc9f9a
ferrerooo <ferrerooo@gmail.com>
2022-11-30 11:05:16 -0800

    update

---
+++ LC_0139_WordBreak.java
@@ -0,0 +1,22 @@
+public class LC_0139_WordBreak {
+    
+    public boolean wordBreak(String s, List<String> wordDict) {
+        
+        boolean[] dp = new boolean[s.length()+1];
+        dp[0] = true;
+        
+        for (int i=1; i<dp.length; i++) {
+            
+            for (int j=i; j>0; j--) {
+                
+                if (dp[j-1] && wordDict.contains(s.substring(j-1, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return dp[dp.length-1];
+    }
+
+}

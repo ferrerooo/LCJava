a19edeb5596de967086e27b46fb3ef6e843b9631
ferrerooo <ferrerooo@gmail.com>
2023-01-30 08:31:07 -0800

    update

--- Program.java
+++ Program.java
@@ -8,10 +8,12 @@ public class Program {
     public static void main(String[] args){
         
         LC_0518_CoinChangeII_DFS_TLE lc = new LC_0518_CoinChangeII_DFS_TLE();
+        String str = "a.b.c...";
+        String[] arr = str.split("\\.");
+        System.out.println(arr.length);
 
-        BitSet bs = new BitSet(3);
-
-        System.out.println(bs.nextClearBit(4));
+        for (String s:arr)
+            System.out.println("--"+s);
      }
 
 }

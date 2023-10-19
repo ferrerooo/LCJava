93ec11a9bc78d242ad6a1d61970c269240b66b5f
ZCY <ferrerooo@gmail.com>
2023-10-19 08:32:34 -0700

    Update LC_0027_RemoveElement.java

--- LC_0027_RemoveElement.java
+++ LC_0027_RemoveElement.java
@@ -1,25 +1,25 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         
-        if (nums == null || nums.length == 0)
-            return 0;
-
         int p1 = 0;
-        int p2 = nums.length-1;
+        int p2 = nums.length - 1;
 
         while (p1 <= p2) {
-
-            if (nums[p1] != val) {
-                p1++;
+            if (nums[p1] == val) {
+                swap(nums, p1, p2);
+                p2 --;
             } else {
-
-                nums[p1] = nums[p2];
-                nums[p2] = val;
-                p2--;
+                p1 ++;
             }
-
         }
 
         return p2+1;
+    }
+
+    private void swap(int[] arr, int p1, int p2) {
+        int temp = arr[p1];
+        arr[p1] = arr[p2];
+        arr[p2] = temp;
+        return;
     }
 }

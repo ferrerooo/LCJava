1fe1ceb493122130a7d7790b1e27f73c842291cc
ZCY <ferrerooo@gmail.com>
2023-10-25 05:38:47 -0700

    Create LC_0150_EvaluateReversePolishNotation.java

---
+++ LC_0150_EvaluateReversePolishNotation.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+
+        Stack<String> stack = new Stack<>();
+
+        for (int i=0; i<tokens.length; i++) {
+            
+            if (tokens[i].equals("+")) {
+                int a = Integer.parseInt( stack.pop() );
+                int b = Integer.parseInt( stack.pop() );
+                stack.push(String.valueOf(a+b));
+            } else if (tokens[i].equals("-")) {
+                int a = Integer.parseInt( stack.pop() );
+                int b = Integer.parseInt( stack.pop() );
+                stack.push(String.valueOf(b-a));
+            } else if (tokens[i].equals("*")) {
+                int a = Integer.parseInt( stack.pop() );
+                int b = Integer.parseInt( stack.pop() );
+                stack.push(String.valueOf(a*b));
+            } else if (tokens[i].equals("/")) {
+                int a = Integer.parseInt( stack.pop() );
+                int b = Integer.parseInt( stack.pop() );
+                stack.push(String.valueOf(b/a));
+            } else {
+                stack.push(tokens[i]);
+            }
+
+        }
+
+        String res = stack.peek();
+        return Integer.parseInt(res);
+            
+    }
+}

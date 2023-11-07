9c180c807619217f57a3a1ce6ebfd013069badb6
ZCY <ferrerooo@gmail.com>
2023-11-06 17:20:14 -0800

    Update LC_1086_HighFive.java

--- LC_1086_HighFive.java
+++ LC_1086_HighFive.java
@@ -1,40 +1,42 @@
 class Solution {
     public int[][] highFive(int[][] items) {
 
-        Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
+        Map<Integer, PriorityQueue<Integer>> idScoresMap = new HashMap<>();
 
         for (int[] item : items) {
 
-            int id = item[0];
-            int score = item[1];
-            //System.out.println("init-id:"+id + "score:"+score);
-            if (!map.containsKey(id)) 
-                map.put(id, new PriorityQueue<Integer>((x,y)->y-x));
-            map.get(id).offer(score);
-            //System.out.println("id:"+id + "score:"+score);
+            if (idScoresMap.containsKey(item[0])) {
+                idScoresMap.get(item[0]).offer(item[1]);
+            } else {
+                PriorityQueue<Integer> pq = new PriorityQueue<Integer>((x,y)->(y-x));
+                pq.offer(item[1]);
+                idScoresMap.put(item[0], pq);
+            }
         }
 
-
-        int[][] res = new int[map.size()][2];
+        int[][] results = new int[idScoresMap.size()][2];
         int index = 0;
-        for (Map.Entry<Integer, PriorityQueue<Integer>> entry : map.entrySet()) {
 
-            res[index][0] = entry.getKey();
-            PriorityQueue<Integer> pqueue = entry.getValue();
-            //System.out.println("id:"+res[index][0]+", score size:"+pqueue.size());
-            
-            int sum = 0;
-            for(int i=0; i<5; i++) {
-                int score = pqueue.poll();
-                sum = sum + score;
-            }
+        for (Map.Entry<Integer, PriorityQueue<Integer>> entry : idScoresMap.entrySet()) {
 
-            res[index][1] = sum/5;
+            results[index][0] = entry.getKey();
+            results[index][1] = getTop5Avg(entry.getValue());
+
             index++;
         }
 
-        Arrays.sort(res, (x,y)->(x[0]-y[0]));
+        return results;
         
-        return res;
+    }
+
+    private int getTop5Avg(PriorityQueue<Integer> pq) {
+
+        int sum = 0;
+        for (int i=0;i<5;i++) {
+            sum += pq.poll();
+        }
+
+        return sum/5;
+
     }
 }

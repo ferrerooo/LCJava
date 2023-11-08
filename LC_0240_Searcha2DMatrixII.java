669520937c2698cdfd5297edf527faa60983837d
ZCY <ferrerooo@gmail.com>
2023-11-08 13:52:09 -0800

    Update LC_0240_Searcha2DMatrixII.java

--- LC_0240_Searcha2DMatrixII.java
+++ LC_0240_Searcha2DMatrixII.java
@@ -23,3 +23,41 @@ public class LC_0240_Searcha2DMatrixII {
         return true;
     }
 }
+
+
+/*
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        int x = row-1;
+        int y = 0;
+
+        while (isOnBoard(x, y, row, col)) {
+
+            if (matrix[x][y] == target) {
+                return true;
+            }
+
+            if (matrix[x][y] > target) {
+                x -= 1;
+            } else {
+                y += 1;
+            }
+
+        }
+
+        return false;
+    }
+
+    private boolean isOnBoard(int x, int y, int row, int col) {
+        if (x>=0 && x<row && y>=0 && y<col) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+*/

c6fc26c9ac179361d40f491f1cc5843a3acc4e9c
ZCY <ferrerooo@gmail.com>
2023-11-09 00:24:21 -0800

    Update LC_1438_LongestContinuousSubarrayWithAbsoluteDiff_monoQ.java

--- LC_1438_LongestContinuousSubarrayWithAbsoluteDiff_monoQ.java
+++ LC_1438_LongestContinuousSubarrayWithAbsoluteDiff_monoQ.java
@@ -40,3 +40,69 @@ public class LC_1438_LongestContinuousSubarrayWithAbsoluteDiff_monoQ {
 
     }
 }
+
+/*
+
+class Solution {
+    public int longestSubarray(int[] nums, int limit) {
+
+        int len = nums.length;
+
+        Deque<Integer> maxdq = new LinkedList<>();
+        Deque<Integer> mindq = new LinkedList<>();
+
+        System.out.println("limit is : " + limit);
+        int start = 0;
+        int res = 0;
+
+        for (int i=0 ;i<len; i++) {
+
+            while (maxdq.size() > 0 && nums[maxdq.peekLast()] < nums[i]) {
+                maxdq.pollLast();
+            }
+
+            maxdq.offerLast(i);
+
+            while (mindq.size() > 0 && nums[mindq.peekLast()] > nums[i]) {
+                mindq.pollLast();
+            }
+
+            mindq.offerLast(i);
+
+            if (nums[maxdq.peekFirst()] - nums[mindq.peekFirst()] <= limit) {
+                res = Math.max(res, i-start+1);
+            }
+
+            while (nums[maxdq.peekFirst()] - nums[mindq.peekFirst()] > limit) {
+
+                System.out.println("start:"+start);
+
+                if (nums[start] < nums[maxdq.peekFirst()] && nums[start] > nums[mindq.peekFirst()]) {
+                    System.out.println("case1");
+                    System.out.println("maxdq.peekFirst():"+maxdq.peekFirst());
+                    System.out.println("mindq.peekFirst():"+mindq.peekFirst());
+                    start ++;
+                    continue;
+                } 
+
+                if (nums[start] == nums[maxdq.peekFirst()]) {
+                    System.out.println("case2");
+                    start ++;
+                    maxdq.pollFirst();
+                    continue;
+                }
+
+                if (nums[start] == nums[mindq.peekFirst()]) {
+                    System.out.println("case3");
+                    start ++;
+                    mindq.pollFirst();
+                    continue;
+                }
+            }
+        }
+
+        return res;
+        
+    }
+}
+*/

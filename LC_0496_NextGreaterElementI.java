14f7f2acb71c990f5d7664abb00b2f13a57b025e
ZCY <ferrerooo@gmail.com>
2023-11-20 22:05:12 -0800

    Update LC_0496_NextGreaterElementI.java

--- LC_0496_NextGreaterElementI.java
+++ LC_0496_NextGreaterElementI.java
@@ -24,24 +24,3 @@ public class LC_0496_NextGreaterElementI {
 
     }
 }
-
-/*
-class Solution:
-    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        d = {}
-        stack = []
-        for n in nums2:
-            while len(stack) > 0 and n > stack[-1]:
-                d[stack.pop()] = n
-            stack.append(n)
-        
-        answer = []
-        for n in nums1:
-            if n in d:
-                answer.append(d[n])
-            else:
-                answer.append(-1)
-            
-        return answer
-
-*/
